9fe6024ddebb7240919ce7c07ecf9f2b24bd852b
Jack M <jackmurphyaz@gmail.com>
2026-06-01 22:18:19 -0700

    feat: add rough working logic for tic-tac-toe with messagebox that displays when a user wins the game and a reset of the board after a win

--- testing/jack/jack.py
+++ testing/jack/jack.py
@@ -1,52 +1,113 @@
 from tkinter import *
 from tkinter import ttk
+from tkinter import messagebox
 root = Tk()
 frm = ttk.Frame(root, padding=10)
 frm.grid()
 
-
 def makeTicBox(col: int, row: int) -> Listbox:
     box = Listbox(root, borderwidth=0, background='white')
     box.grid(column=col, row=row)
-    box.IsPressed = False
-    #box.Symbol = ""
+    box.row = row
+    box.col = col
+    box.isPressed = False
+    box.symbol = ""
 
     return box
 
 boxList = [None] * 3
 
+
 for row in range(3):
     boxList[row] = [None] * 3
 
     for col in range(3):
-        boxList[row][col] = makeTicBox(col,row)
+        boxList[row][col] = makeTicBox(col, row)
 
 
 
 options = ["X", "O"]
 currOption = 0
-
+ 
 def turn(event):
     global currOption
-
     
-    if(not event.widget.IsPressed):
+    if(event.widget.isPressed == False):
+        event.widget.isPressed = True
         event.widget["background"] = "gray"
-        event.widget.IsPressed = True
-        event.widget.insert(END, options[currOption])
+        symbol = options[currOption]
+        event.widget.insert(END, symbol)
+        event.widget.symbol = symbol
         currOption = (currOption + 1) % 2
 
-    
+        if(checkWin(symbol, event.widget.row, event.widget.col) == True):
+            messagebox.showinfo("Congrats!", f"{symbol} has won!")
+            reset()
+        
 
-    # 0 // 2 -> 0 0
-    # 1 // 2 -> 0 1
-    # 2 // 2 -> 1 0
-    # 3 // 2 -> 1 1
-    # 4 // 2 -> 2 0
+        #maybe handle logic in here? make options[currOption] a variable to display winner?
 
+
+#only runs once at the beginning
 for boxCol in boxList:
     for box in boxCol:
         box.bind("<Button>", turn)
+        
+def checkWin(symbol: str, row: int, col: int) -> bool:
+    #if [0][0], [1][1], or [2][2] check diagRight
+    if((row == 0 and col == 0) or (row == 1 and col == 1) or (row == 2 and col == 2)):
+        if(diagonalRight(symbol) == True):
+            return True
+
+    #if [0][2], [1][1], or [2][0] check diagLeft
+    if((row == 0 and col == 2) or (row == 1 and col == 1) or (row == 2 and col == 0)):
+        if(diagonalLeft(symbol) == True):
+            return True
+    
+    if(down(symbol, col) == True):
+        return True
+    if(right(symbol, row) == True):
+        return True
+
+    print("checkWin returned false")
+    return False
+
+def diagonalRight(symbol: str) -> bool:
+    for index in range(3):
+        if(boxList[index][index].symbol != symbol):
+            return False
+    return True
+
+def diagonalLeft(symbol: str) -> bool:
+    verdict = False
+
+    if((boxList[0][2].symbol == symbol) and (boxList[1][1].symbol == symbol) and (boxList[2][0].symbol == symbol)):
+        verdict = True
+    return verdict
+
+def down(symbol: str, col: int) -> bool:
+    for i in range(3):
+        if(boxList[i][col].symbol != symbol):
+            return False
+    return True
+
+def right(symbol: str, row: int) -> bool:
+    for i in range(3):
+        if(boxList[row][i].symbol != symbol):
+            return False
+    return True
+
+
+def reset():
+    for row in range(3):
+        boxList[row] = [None] * 3
+
+        for col in range(3):
+            boxList[row][col] = makeTicBox(col, row)
+
+    for boxCol in boxList:
+        for box in boxCol:
+            box.bind("<Button>", turn)
        
 
 ttk.Button(frm, text="Quit", command=root.destroy).grid(column=1, row=10)
